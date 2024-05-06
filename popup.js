4f12d42243124aad0f9e24afce5e48f9437bb424
Leo Ossa <leonard@leonardossa.com>
2024-05-06 13:08:21 +0200

    style,refactor: format popup.js

--- popup.js
+++ popup.js
@@ -1,46 +1,35 @@
-document.addEventListener('DOMContentLoaded', () =>
-{
+document.addEventListener('DOMContentLoaded', () => {
   addEventListeners();
   populateUI();
 });
 
-function addEventListeners()
-{
+function addEventListeners() {
   //add open options button event listener
-  document.querySelector('#options').addEventListener('click', function ()
-  {
-    if (chrome.runtime.openOptionsPage)
-    {
+  document.querySelector('#options').addEventListener('click', function () {
+    if (chrome.runtime.openOptionsPage) {
       chrome.runtime.openOptionsPage();
-    } else
-    {
+    } else {
       window.open(chrome.runtime.getURL('options.html'));
     }
   });
 
-  document.querySelector('#refresh').addEventListener('click', function ()
-  {
+  document.querySelector('#refresh').addEventListener('click', function () {
     fetch_and_save_boards();
     populateUI();
   });
 
   //add get decks button event listener
-  document.getElementById('sendPage').addEventListener('click', function (event)
-  {
+  document.getElementById('sendPage').addEventListener('click', function (event) {
     const sendPageButton = document.getElementById('sendPage');
-    chrome.storage.sync.get(["debug_mode", "defaultBoardId", "defaultStackId", "archive"], (items) =>
-    {
+    chrome.storage.sync.get(["debug_mode", "defaultBoardId", "defaultStackId", "archive"], (items) => {
       if (items.defaultStackId) // if there's no selected stack to send then there's no point of making call
       {
-        chrome.tabs.query({ active: true, currentWindow: true }, currentTab => 
-        {
-          if (items.archive)
-          {
+        chrome.tabs.query({ active: true, currentWindow: true }, currentTab => {
+          if (items.archive) {
             archivePage(currentTab[0].url);
           }
           if (items.debug_mode) console.log("current tab is:", currentTab);
-          create_new_card(items.defaultBoardId, items.defaultStackId, currentTab[0].title, 1, currentTab[0].url, ((data) =>
-          {
+          create_new_card(items.defaultBoardId, items.defaultStackId, currentTab[0].title, 1, currentTab[0].url, ((data) => {
             sendPageButton.classList.toggle('fadeOut');
             setTimeout(function () { sendPageButton.remove(); }, 1000);
           }));
